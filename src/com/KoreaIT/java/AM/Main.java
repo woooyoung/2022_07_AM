41958faff506ebf695a63589a69c9f2bc370f6d5
woooyoung <axdsw121@gmail.com>
2022-08-09 15:51:09 +0900

    여러가지 명령어 추가 연습

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -15,6 +15,16 @@ public class Main {
 			if (cmd.equals("exit")) {
 				break;
 			}
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
 
 		}
 
