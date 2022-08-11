09c6588e855c7fe5405ee707ba0778007386c702
woooyoung <axdsw121@gmail.com>
2022-08-11 14:52:02 +0900

    게시물 삭제 시도중

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -73,6 +73,32 @@ public class Main {
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+			} else if (cmd.startsWith("article delete ")) {
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				// size() => 3
+				// index : 0,1,2
+				// id : 1,2,3
+
+				articles.remove(id - 1);
+				System.out.printf("%d번 글을 삭제했습니다.\n", id);
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
