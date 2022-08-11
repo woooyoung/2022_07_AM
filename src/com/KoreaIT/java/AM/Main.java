bc9fa1d9d2927169a1581b6983895664dff1f8bf
woooyoung <axdsw121@gmail.com>
2022-08-11 13:39:56 +0900

    게시물 상세보기(글이 존재하는지 아닌지 알고 있는 상태) 구현중

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -49,7 +49,28 @@ public class Main {
 					System.out.printf("%4d    |    %s\n", article.id, article.title);
 				}
 			} else if (cmd.startsWith("article detail ")) {
-				System.out.printf("%d번 게시글은 존재하지 않습니다\n", 1);
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]); // "2" -> 2
+
+				boolean found = false;
+
+				for (int i = 0; i < articles.size(); i++) { // 0~4
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						found = true;
+						break;
+					}
+				}
+
+				if (found == false) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				} else {
+					System.out.printf("%d번 게시물은 존재합니다\n", id);
+				}
+
 			} else {
 
 				System.out.println("존재하지 않는 명령어입니다");
