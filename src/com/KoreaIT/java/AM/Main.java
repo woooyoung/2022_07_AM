9a6ca1e7f9ed765e62c9aa5dd137bac565af8cc8
woooyoung <axdsw121@gmail.com>
2022-08-11 14:22:58 +0900

    게시물 작성시 작성 날짜와 시간을 저장

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -28,12 +28,13 @@ public class Main {
 			if (cmd.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.getDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -69,7 +70,7 @@ public class Main {
 					continue;
 				}
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", "12-12-12 12:12:12");
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 			} else {
@@ -86,11 +87,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	Article(int id, String title, String body) {
+	Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
---
+++ src/com/KoreaIT/java/AM/Util.java
@@ -0,0 +1,15 @@
+package com.KoreaIT.java.AM;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	//현재 날짜 시간
+	public static String getDateStr() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date time = new Date();
+
+		return format1.format(time);
+	}
+}
